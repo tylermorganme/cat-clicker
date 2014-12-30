a03d7755a56e2c25a893a44fe79c345bfc0b5801
Tyler Morgan <tyler@tylermorgan.me>
2014-12-29 18:42:24 -0700

    added cat list and now display a single cat at a time

--- js/data.js
+++ js/data.js
@@ -8,8 +8,29 @@ var catData = [
 	},
 	{
 		id: "keith",
-		img: "https://placekitten.com/g/200/300",
+		img: "https://placekitten.com/g/800/300",
 		name: "Keith",
+		link: "",
+		clicks: 0 
+	},
+	{
+		id: "oz",
+		img: "https://placekitten.com/g/500/500",
+		name: "Oz",
+		link: "",
+		clicks: 0 
+	},
+	{
+		id: "Nickodemus",
+		img: "https://placekitten.com/g/200/200",
+		name: "Nick",
+		link: "",
+		clicks: 0 
+	},
+	{
+		id: "Alabaster",
+		img: "https://placekitten.com/g/300/300",
+		name: "Al",
 		link: "",
 		clicks: 0 
 	}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-var catInfo = '<div id="{{id}}"><h1>{{name}}</h1><img src="{{img}}"><p>{{name}} has been clicked: <span>0</span> times!</p></div>'
-
-
+var catInfo = '<div id="{{id}}"><h1>{{name}}</h1><p>has been clicked: <span>{{clicks}}</span> times!</p><img src="{{img}}"></div>'
+var totalClicks = 0;
 var catClicker = function(id, num) {
-		$("#"+id).click(function(){
-			catData[num]["clicks"] +=1;
-			$("#" + id + ' span').text(catData[num]["clicks"]);
-			console.log(id + " has been clicked");
+	$("#"+id).click(function(){
+		catData[num]["clicks"] +=1;
+		totalClicks +=1;
+		$("#" + id + ' span').text(catData[num]["clicks"]);
+		$("#total-clicks span").text(totalClicks);
 	});
 };
 
-for (var cat in catData) {
-	$('#cat-area').append(catInfo
-		.replace("{{id}}", catData[cat]["id"])	
-		.replace("{{name}}", catData[cat]["name"])
-		.replace("{{name}}", catData[cat]["name"])
-		.replace("{{img}}", catData[cat]["img"])
+var catSelectClicker = function(num) {
+	$("#cat-names li").eq(num).click(function(){
+		changeCat(num);
+	});
+};
+
+var displayCat = function(num) {
+	$('#cat-showcase').append(catInfo
+		.replace("{{id}}", catData[num]["id"])
+		.replace("{{clicks}}", catData[num]["clicks"])	
+		.replace("{{name}}", catData[num]["name"])
+		.replace("{{img}}", catData[num]["img"])
 	);
-	catClicker(catData[cat]["id"], cat);
+	catClicker(catData[num]["id"], num);
 }
+
+var displayCatList = function(){
+	for (var cat in catData) {
+		$('#cat-names ul').append("<li>" + catData[cat]["name"] + "</li>");
+		catSelectClicker(cat);
+	}
+}
+
+var changeCat = function(num) {
+	$('#cat-showcase').html('');
+	displayCat(num);
+}
+
+displayCatList();
+
