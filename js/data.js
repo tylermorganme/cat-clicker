685a8a6a002abf001acf7a71b704c6659c7bcfdf
Tyler Morgan <tyler@tylermorgan.me>
2014-12-31 15:05:29 -0700

    refactored to MV*. List view works correctly. Single cat view works correctly. Clicks do not record

--- js/data.js
+++ js/data.js
@@ -1,40 +1,94 @@
-var catData = [
-	{
-		id: "toby",
-		img: "https://placekitten.com/g/200/300",
-		name: "Toby",
-		link: "",
-		clicks: 0  
-	},
-	{
-		id: "keith",
-		img: "https://placekitten.com/g/800/300",
-		name: "Keith",
-		link: "",
-		clicks: 0 
-	},
-	{
-		id: "oz",
-		img: "https://placekitten.com/g/500/500",
-		name: "Oz",
-		link: "",
-		clicks: 0 
-	},
-	{
-		id: "Nickodemus",
-		img: "https://placekitten.com/g/200/200",
-		name: "Nick",
-		link: "",
-		clicks: 0 
-	},
-	{
-		id: "Alabaster",
-		img: "https://placekitten.com/g/300/300",
-		name: "Al",
-		link: "",
-		clicks: 0 
+$(function(){
+	var model = {
+		catData:[
+			{
+				id: "toby",
+				img: "https://placekitten.com/g/200/300",
+				name: "Toby",
+				link: "",
+				clicks: 0  
+			},
+			{
+				id: "keith",
+				img: "https://placekitten.com/g/800/300",
+				name: "Keith",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "oz",
+				img: "https://placekitten.com/g/500/500",
+				name: "Oz",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "Nickodemus",
+				img: "https://placekitten.com/g/200/200",
+				name: "Nick",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "Alabaster",
+				img: "https://placekitten.com/g/300/300",
+				name: "Al",
+				link: "",
+				clicks: 0 
+			}	
+		],
+		catInfo: '<div id="{{id}}"><h1>{{name}}</h1><p>has been clicked: <span>{{clicks}}</span> times!</p><img src="{{img}}"></div>',
+		totalClicks: 0
+	};
+
+	var controller = {
+
+	};
+
+	var view = {
+
+	};
+
+	var catClicker = function(id, num) {
+		$("#"+id).click(function(){
+			model.catData[num]["clicks"] +=1;
+			model.totalClicks +=1;
+			$("#" + id + ' span').text(model.catData[num]["clicks"]);
+			$("#total-clicks span").text(model.totalClicks);
+		});
+	};
+
+	var catSelectClicker = function(num) {
+		$("#cat-names li").eq(num).click(function(){
+			changeCat(num);
+		});
+	};
+
+	var displayCat = function(num) {
+		$('#cat-showcase').append(catInfo
+			.replace("{{id}}", model.catData[num]["id"])
+			.replace("{{clicks}}", model.catData[num]["clicks"])	
+			.replace("{{name}}", model.catData[num]["name"])
+			.replace("{{img}}", model.catData[num]["img"])
+		);
+		catClicker(model.catData[num]["id"], num);
 	}
 
-];
+	var displayCatList = function(){
+		for (var cat in model.catData) {
+			$('#cat-names ul').append("<li>" + model.catData[cat]["name"] + "</li>");
+			catSelectClicker(cat);
+		}
+	}
+
+	var changeCat = function(num) {
+		$('#cat-showcase').html('');
+		displayCat(num);
+	}
+
+	displayCatList();	
+});
+
+
 
 
--- js/main.js
+++ js/main.js
@@ -1,41 +1,97 @@
-var catInfo = '<div id="{{id}}"><h1>{{name}}</h1><p>has been clicked: <span>{{clicks}}</span> times!</p><img src="{{img}}"></div>'
-var totalClicks = 0;
-var catClicker = function(id, num) {
-	$("#"+id).click(function(){
-		catData[num]["clicks"] +=1;
-		totalClicks +=1;
-		$("#" + id + ' span').text(catData[num]["clicks"]);
-		$("#total-clicks span").text(totalClicks);
-	});
-};
+$(function(){
+	var controller = {
+		activeCat: 0,
+		init: function() {
+			viewCatList.displayAll(model.catData);
+			viewSingleCat.display(model.catData[this.activeCat]);
+		},
+		getCats: function() {
+			return model.catData;
+		},
+		changeCat: function(num) {
+			console.log(num);
+			viewSingleCat.display(model.catData[num]);
+		}
+	};
 
-var catSelectClicker = function(num) {
-	$("#cat-names li").eq(num).click(function(){
-		changeCat(num);
-	});
-};
+	var viewSingleCat = {
+		catInfo: '<div id="{{id}}"><h1>{{name}}</h1><p>has been clicked: <span>{{clicks}}</span> times!</p><img src="{{img}}"></div>',
+		display: function(cat) {
+			var showcase = $('#cat-showcase')
+			showcase.html('');
+			showcase.append(viewSingleCat.catInfo
+				.replace("{{id}}", cat["id"])
+				.replace("{{clicks}}", cat["clicks"])	
+				.replace("{{name}}", cat["name"])
+				.replace("{{img}}", cat["img"])
+			);
+		},
+	};
 
-var displayCat = function(num) {
-	$('#cat-showcase').append(catInfo
-		.replace("{{id}}", catData[num]["id"])
-		.replace("{{clicks}}", catData[num]["clicks"])	
-		.replace("{{name}}", catData[num]["name"])
-		.replace("{{img}}", catData[num]["img"])
-	);
-	catClicker(catData[num]["id"], num);
-}
+	var viewCatList = {
+		displayAll: function(cats){
+			var $catListElem = $('#cat-names ul');
+			for (var cat in cats) {
+				$catListElem.append("<li>" + cats[cat]["name"] + "</li>");
+			}
+			$('#cat-names li').each(function(index){
+				$(this).click(function(){
+					controller.changeCat(index);
+				});
+			});
+		},
+	};
 
-var displayCatList = function(){
-	for (var cat in catData) {
-		$('#cat-names ul').append("<li>" + catData[cat]["name"] + "</li>");
-		catSelectClicker(cat);
-	}
-}
+	var viewTotal = {
+		display: function() {
+			$("#total-clicks span").text(model.totalClicks);
+		}
+	};
 
-var changeCat = function(num) {
-	$('#cat-showcase').html('');
-	displayCat(num);
-}
+	var model = {
+		catData:[
+			{
+				id: "toby",
+				img: "https://placekitten.com/g/200/300",
+				name: "Toby",
+				link: "",
+				clicks: 0  
+			},
+			{
+				id: "keith",
+				img: "https://placekitten.com/g/800/300",
+				name: "Keith",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "oz",
+				img: "https://placekitten.com/g/500/500",
+				name: "Oz",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "Nickodemus",
+				img: "https://placekitten.com/g/200/200",
+				name: "Nick",
+				link: "",
+				clicks: 0 
+			},
+			{
+				id: "Alabaster",
+				img: "https://placekitten.com/g/300/300",
+				name: "Al",
+				link: "",
+				clicks: 0 
+			}	
+		],
+		totalClicks: 0
+	};
 
-displayCatList();
+	controller.init();	
+});
+
+
+
 
