891e62ee2c184db8c07501aac0b4ccd8caa843cb
Tyler Morgan <tyler@tylermorgan.me>
2015-01-06 20:34:43 -0700

    implemented admin menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 $(function(){
 	var controller = {
 		init: function() {
+			model.setActiveCat(0);
 			viewCatList.displayAll(model.catData);
 			viewSingleCat.display(model.catData[0], 0);
+			viewAdmin.init();
 		},
 		getCats: function() {
 			return model.catData;
 		},
 		changeCat: function(num) {
-			viewSingleCat.display(model.catData[num], num);
+			model.setActiveCat(num);
+			viewSingleCat.display(model.getActiveCat(num), num);
 		},
-		catClick: function(num) {
+		catClick: function(id) {
 			model.totalClicks += 1;
 			viewTotal.display();
-			model.catData[num]["clicks"] += 1;
-			viewSingleCat.updateClicks(model.catData[num]["clicks"]);
+			model.addClick(id);
+			viewSingleCat.updateClicks(model.catData[id]["clicks"]);
+			viewAdmin.updateClicks(model.activeCat);
+		},
+		getActiveCat: function() {
+			return model.activeCat;
+		},
+		getTotalClicks: function() {
+			return model.totalClicks;
+		},
+		submitCat: function(cat) {
+			model.setCat(cat);
+			model.setActiveCat(model.activeCat.num);
+			viewSingleCat.display(cat, model.activeCat.num);
+			viewCatList.displayAll(model.catData);
+			model.updateTotalClicks();
+			viewTotal.display();
 		}
 	};
 
@@ -34,19 +52,21 @@ $(function(){
 			});
 		},
 		updateClicks: function(num) {
-			$('#cat-showcase span').text(num)
+			$('#cat-showcase span').text(num);
 		}
 	};
 
 	var viewCatList = {
 		displayAll: function(cats){
 			var $catListElem = $('#cat-names ul');
+			$catListElem.html('');
 			for (var cat in cats) {
 				$catListElem.append("<li>" + cats[cat]["name"] + "</li>");
 			}
 			$('#cat-names li').each(function(index){
 				$(this).click(function(){
 					controller.changeCat(index);
+					viewAdmin.update(controller.getActiveCat());
 				});
 			});
 		},
@@ -54,9 +74,43 @@ $(function(){
 
 	var viewTotal = {
 		display: function() {
-			$("#total-clicks span").text(model.totalClicks);
+			$("#total-clicks span").text(controller.getTotalClicks());
 		}
 	};
+
+	var viewAdmin = {
+		init: function() {
+			$('#btn-admin').click(function(){
+				$('#admin-interface').toggle();
+				viewAdmin.update(controller.getActiveCat());
+			});
+			$('#btn-cancel').click(function(event){
+				$('#admin-interface').hide();
+				event.preventDefault();
+			});
+			$('#btn-submit').click(function(event){
+				$('#admin-interface').hide();
+				controller.submitCat({
+					id: $('#id').val(),
+					img: $('#image').val(),
+					name: $('#name').val(),
+					link: $('#link').val(),
+					clicks: parseInt($('#clicks').val())
+				});
+				event.preventDefault();
+			});
+		},
+		update: function(cat) {
+			$('#id').val(cat.id);
+			$('#image').val(cat.img);
+			$('#name').val(cat.name);
+			$('#link').val(cat.link);
+			$('#clicks').val(cat.clicks);
+		},
+		updateClicks: function(cat) {
+			$('#clicks').val(cat.clicks);
+		}
+	}
 
 	var model = {
 		catData:[
@@ -82,21 +136,49 @@ $(function(){
 				clicks: 0 
 			},
 			{
-				id: "Nickodemus",
+				id: "nickodemus",
 				img: "https://placekitten.com/g/200/200",
 				name: "Nick",
 				link: "",
 				clicks: 0 
 			},
 			{
-				id: "Alabaster",
+				id: "alabaster",
 				img: "https://placekitten.com/g/300/300",
 				name: "Al",
 				link: "",
 				clicks: 0 
 			}	
 		],
-		totalClicks: 0
+		totalClicks: 0,
+		updateTotalClicks: function() {
+			var result = 0;
+			for (var cat in model.catData) {
+				result += model.catData[cat]["clicks"];
+			}
+			model.totalClicks = result;
+		},
+		setActiveCat: function(num) {
+			model.activeCat = model.catData[num];
+			model.activeCat.num = num;
+		},
+		getActiveCat: function() {
+			return model.activeCat;
+		},
+		findByID: function() {
+			for (var cat in model.catData) {
+				if (model.catData[cat]["id"] == id) {
+					return cat;
+					break;
+				}
+			}
+		},
+		addClick: function(id) {
+			model.activeCat["clicks"] += 1;
+		},
+		setCat: function(cat) {
+			model.catData[model.activeCat.num] = cat;
+		}
 	};
 
 	controller.init();	
